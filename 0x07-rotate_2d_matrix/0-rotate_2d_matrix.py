0af355ed7a22d86034a278be0259ff63335d48ee
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-02-22 22:08:16 +0300

    feat: matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i, n):
+            matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+
+    # Reverse each row
+    for i in range(n):
+        for j in range(n // 2):
+            matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
