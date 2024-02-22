1cf171fe0b749e8a3c6c432267e027e8254a3715
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-02-22 22:13:58 +0300

    feat: matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def rotate_2d_matrix(matrix):
+    """
+    rotate_2d_matrix - Rotates a 2D matrix 90 degrees clockwise
+    """
     n = len(matrix)
 
     # Transpose the matrix
