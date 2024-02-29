d84f7c1c0f3b61368eab7aea13265e29c7b6298b
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-02-29 21:31:52 +0300

    feat: Change making problem

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!usr/bin.python3
+"""
+Coin change making problem
+"""
+
+
+def makeChange(coins, total):
+    """
+    Make change for a given total using the fewest number of coins
+    """
+    if total <= 0:
+        return 0
+
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return -1 if dp[total] == total + 1 else dp[total]
